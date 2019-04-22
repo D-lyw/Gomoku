d02d41a01b5db43234b9b8f1c9b0cfe34f64a189
ciuny <961831239@qq.com>
2019-04-22 22:27:45 +0800

    实现布局的大致轮廓

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -6,18 +6,25 @@
 
 <script>
   import home from './components/home/home';
-export default {
-  name: 'App',
-  components:{
-    home,
-  }
-}
+
+  export default {
+    name: 'App',
+    components: {
+      home,
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+  }
 </style>
