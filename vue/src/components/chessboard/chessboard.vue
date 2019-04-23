ffb5e5584baafe8b49d7399af9e4ab1b2e1beae5
tongtong <1053673440@qq.com>
2019-04-23 12:33:33 +0800

    '添加了棋盘'

--- vue/src/components/chessboard/chessboard.vue
+++ vue/src/components/chessboard/chessboard.vue
@@ -1,17 +1,88 @@
 <template>
-    <div id="chessBoard">
-      棋盘区
+  <div id="app">
+    <div class="row" v-for="(rowData, rowIndex) in map" :key="rowIndex">
+      <div class="col" v-for="(colData, colIndex) in rowData" :key="colIndex" @click="doChess(rowIndex, colIndex)">
+        <span v-if="colData === 1" class="white"></span>
+        <span v-else-if="colData === -1" class="black"></span>
+        <span v-else ></span>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'chess-board',
-  };
+export default {
+  name: "chess-board",
+  data() {
+    return {
+      map: [
+        [1, 0, 1, -1, 0, -1, 0, 1, -1, -1, 1, -1, 1, 1],
+        [-1, -1, 1, 1, 0, 0, -1, 1, -1, -1,0, 0, 1, -1],
+        [1, 1, 0, -1, 1, -1, 0, 1, -1, -1, -1, 1, 1, 1],
+        [-1, 1, 1, 1, 0, -1, -1, 1, 1, -0, 1, -1, -1, 0],
+        [1, 0, 1, -1, 1, 1, -1, 1, -1, -1, 0, -1, 1, 1],
+        [0, -1, 1, -1, 0, -1, 1, 1, 0, 1, 1, -1, 0, -1],
+        [1, -1, 1, 1, 1, 1, -0, 1, -1, -1, 1, -1, 1, 0],
+        [-1, -1, 1, -1, 1, -1, -1, 1, -1, 0,0, -1, 1, -1],
+        [1, 1, 1, 0, 1, -1, -1, 0, 1, -1, -1, 1, 1, 1],
+        [0, -1, 1, 1, 0, -1, 1, 1, -1, -1, 1, 0, -1, 0],
+        [1, 1, 1, -1, 1, -1, -1, 1, -1, -1, -1, -1, 1, 1],
+        [1, -1, -1, 0, 0, -1, -1, 1, -1, 1,0, -1, -1, -1],
+        [0, -1, 1, -1, 1, -1, 1, 1, -1, -1, -1, -1, 0, 1],
+        [1, -1, 0, 1, 1, -1, -1, 1, 0, -1, 1, 1, 1, 1]
+      ]
+    };
+  },
+  methods: {
+    doChess(row, col) {
+      if(this.map[row][col] === 0) {
+        this.$set(this.map[row], col, 1)
+      }
+    }
+  },
+  mounted() {
+    
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  #chessBoard{
-    width: 600px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  margin: 0 auto;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: bisque;
+  .row {
+    width: 100%;
+    height: 7.15%;
+    display: flex;
+    .col {
+      width: 7.15%;
+      height: 100%;
+      border: 1px solid black;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      span {
+        display: inline-block;
+        width: 90%;
+        height: 90%;
+        border-radius: 50%;
+        // background-color: black;
+      }
+      span.white {
+        background-color: white;
+      }
+      span.black {
+        background-color: black;
+      }
+    }
   }
+}
 </style>
